54e1c25e2dd1c7ac6dfb5849c421f50c3517bdcd
Marc Gueury <marc.gueury@oracle.com>
2024-12-11 17:01:33 +0100

    v1

---
+++ starter/bin/destroy_all.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+if [[ -z "${PROJECT_DIR}" ]]; then
+  echo "Error: PROJECT_DIR not set"
+  exit
+fi
+cd $PROJECT_DIR
+SECONDS=0
+
+# Call the script with --auto-approve to destroy without prompt
+
+. env.sh -no-auto
+title "OCI Starter - Destroy"
+echo 
+echo "Warning: This will destroy all the resources created by Terraform."
+echo 
+if [ "$1" != "--auto-approve" ]; then
+  read -p "Do you want to proceed? (yes/no) " yn
+
+  case $yn in 
+  	yes ) echo Deleting;;
+	no ) echo Exiting...;
+		exit;;
+	* ) echo Invalid response;
+		exit 1;;
+  esac
+fi
+
+. env.sh
+
+# Check if there is something to destroy.
+if [ -f $STATE_FILE ]; then
+  export TF_RESOURCE=`cat $STATE_FILE | jq ".resources | length"`
+  if [ "$TF_RESOURCE" == "0" ]; then
+    echo "No resource in terraform state file. Nothing to destroy."
+    exit 
+  fi
+else
+  echo "File $STATE_FILE does not exist. Nothing to destroy."
+  exit 
+fi
+
+if [ -f $PROJECT_DIR/src/terraform/oke.tf ]; then
+  title "OKE Destroy"
+  bin/oke_destroy.sh --auto-approve
+fi
+
+cleanBucket() {
+  BUCKET_NAME=$1
+  export TF_OBJECT_STORAGE=`cat $STATE_FILE | jq -r '.resources[] | select(.name=="'${BUCKET_NAME}'") | .instances[].attributes.bucket_id'`
+  if [ "$TF_OBJECT_STORAGE" != "" ] && [ "$TF_OBJECT_STORAGE" != "null" ]; then
+    title "Delete Object Storage files"
+    # Could be improved....
+    oci os object bulk-delete -bn ${TF_VAR_prefix}-public-bucket --force
+  else
+    echo "No Object storage $BUCKET_NAME"
+  fi  
+}
+
+cleanBucket ${TF_VAR_prefix}-public-bucket
+cleanBucket ${TF_VAR_prefix}-bucket
+
+title "Terraform Destroy"
+src/terraform/destroy.sh --auto-approve -no-color
+exit_on_error
+
+echo "Destroy time: ${SECONDS} secs"
---
+++ starter/env.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export BIN_DIR=$PROJECT_DIR/bin
+
+# Env Variables
+export TF_VAR_prefix="forward"
+
+export TF_VAR_ui_type="html"
+export TF_VAR_db_type="none"
+export TF_VAR_license_model="LICENSE_INCLUDED"
+export TF_VAR_oke_type="managed"
+export TF_VAR_deploy_type="container_instance"
+export TF_VAR_language="python"
+
+export TF_VAR_compartment_ocid="__TO_FILL__"
+# TF_VAR_auth_token : See doc: https://docs.oracle.com/en-us/iaas/Content/Registry/Tasks/registrygettingauthtoken.htm
+export TF_VAR_auth_token="__TO_FILL__"
+
+if [ -f $PROJECT_DIR/../group_common_env.sh ]; then
+  . $PROJECT_DIR/../group_common_env.sh
+elif [ -f $PROJECT_DIR/../../group_common_env.sh ]; then
+  . $PROJECT_DIR/../../group_common_env.sh
+elif [ -f $HOME/.oci_starter_profile ]; then
+  . $HOME/.oci_starter_profile
+fi
+
+# Creation Details
+export OCI_STARTER_CREATION_DATE=2024-12-11-15-58-35-694429
+export OCI_STARTER_VERSION=2.0
+export OCI_STARTER_PARAMS="prefix,java_framework,java_vm,java_version,ui_type,db_type,license_model,mode,infra_as_code,db_password,oke_type,deploy_type,language"
+
+# Get other env variables automatically (-silent flag can be passed)
+. $BIN_DIR/auto_env.sh $1
