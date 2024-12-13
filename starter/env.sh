f4dd4ef69b203fed9d413bb56fad88f40c093ade
Marc Gueury <marc.gueury@oracle.com>
2024-12-13 10:09:03 +0100

    v1

--- starter/env.sh
+++ starter/env.sh
@@ -5,7 +5,8 @@ export BIN_DIR=$PROJECT_DIR/bin
 # Env Variables
 export TF_VAR_prefix="forward"
 
-export TF_VAR_api_key="12345"
+export TF_VAR_api_key="__TO_FILL__"
+# export TF_VAR_api_key="12345"
 export TF_VAR_ui_type="html"
 export TF_VAR_db_type="none"
 export TF_VAR_license_model="LICENSE_INCLUDED"
