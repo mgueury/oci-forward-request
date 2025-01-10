c7fee8d641c202bb0b30076ba16056c1775d2cee
Marc Gueury <marc.gueury@oracle.com>
2025-01-10 09:07:25 +0100

    v1

--- starter/src/after_done.sh
+++ starter/src/after_done.sh
@@ -4,7 +4,7 @@ echo "curl -i \\"
 echo "    -X POST \\"
 echo "    -H "Key: $TF_VAR_api_key" \\"
 echo "     -d '{"idleTimeoutInSeconds": 3600}'\\"
-echo "     $UI_URL/app/20240531/agentEndpoints/{agentEndpointId}/sessions https://axn22vrpa3g746wlw4hjc7464u.apigateway.eu-frankfurt-1.oci.customer-oci.com/forward/app/20240531/agentEndpoints/{agentEndpointId}/sessions"
+echo "     $UI_URL/app/20240531/agentEndpoints/{agentEndpointId}/sessions"
 echo
 echo "-- OCI Forward Request ------------------------------------------------"
 echo
