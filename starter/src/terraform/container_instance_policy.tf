d2d6ee0251333bbc888fd55cbf1d0eb6eac3d460
Marc Gueury <marc.gueury@oracle.com>
2025-01-11 14:37:08 +0100

    v1

--- starter/src/terraform/container_instance_policy.tf
+++ starter/src/terraform/container_instance_policy.tf
@@ -12,15 +12,36 @@ locals {
   idcs_url = (var.idcs_url!="")?var.idcs_url:data.oci_identity_domains.starter_domains.domains[0].url
 }
 
-# XXX Should be more specific to the container instance id... XXX
+# IDCS
+resource "oci_identity_domains_dynamic_resource_group" "starter_ci_dyngroup" {
+  #Required
+  count          = (var.idcs_domain_name=="Default"?0:1) 
+  provider       = oci.home
+  display_name = "${var.prefix}-ci-dyngroup"
+  idcs_endpoint = local.idcs_url
+  matching_rule  = "ALL {resource.type='computecontainerinstance'}"
+  schemas = ["urn:ietf:params:scim:schemas:oracle:idcs:DynamicResourceGroup"]
+}
+
+# Identity Domain
+resource "oci_identity_dynamic_group" "starter_ci_dyngroup" {
+  count          = (var.idcs_domain_name=="Default"?1:0) 
+  provider       = oci.home    
+  name           = "${var.prefix}-ci-dyngroup"
+  description    = "Starter - All Container Instances"
+  compartment_id = var.tenancy_ocid
+  matching_rule  = "ALL {resource.type='computecontainerinstance'}"
+  freeform_tags = local.freeform_tags
+}
+
 resource "oci_identity_policy" "starter-ci_policy" {
   provider       = oci.home
   name           = "${var.prefix}-ci-policy"
   description    = "Container instance access to OCIR"
   compartment_id = var.tenancy_ocid
   statements = [
-    "allow any-user to read repos in tenancy where ALL {resource.type='computecontainerinstance'}",
-    "allow any-user to manage genai-agent-family in tenancy where ALL {resource.type='computecontainerinstance'}"
+    "allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-ci-dyngroup to read repos in tenancy",
+    "allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-ci-dyngroup to manage genai-agent-family in tenancy"
   ]
   freeform_tags = local.freeform_tags
 }
